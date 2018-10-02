4d69eab5b62c888f7f8a095023ed60c075872653
Dora0 <43771714+Dora0@users.noreply.github.com>
2018-10-02 16:18:08 +0900

    Add files via upload

---
+++ chatbot/src/main/java/com/example/demo/Callback.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@LineMessageHandler
+public class Callback {
+
+	@Autowired
+	private LineMessagingClient lineMessagingClient;
+
+	@EventMapping
+	public void handleText(MessageEvent<TextMessageContent> event) {
+		System.out.println("event: " + event);
+
+		/* LINEのユーザIDを取得 */
+		String userid = event.getSource().getUserId();
+
+		System.err.println("fafa");
+
+
+		/* リクエストの文言を取得 */
+		TextMessageContent tmc = event.getMessage();
+		String text = tmc.getText();
+
+		// Reply用Listの用意
+		List<Message> replylist = new ArrayList<>();
+
+		replylist.add(new TextMessage("おうむ返しします。"));
+		replylist.add(new TextMessage(text));
+
+		/* Line reply */
+		lineMessagingClient
+		.replyMessage(new ReplyMessage(event.getReplyToken(),
+				replylist));
+
+	}
+
+	@EventMapping
+	public void handleEvent(Event event) {
+		System.out.println("event: " + event);
+	}
+
+}
